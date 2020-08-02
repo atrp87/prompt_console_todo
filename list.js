c583dd4042302eda6d3616b740bac2b4a0ac7f49
Andrew Peattie <drewpeattie@hotmail.com>
2020-08-02 13:16:23 +0100

    add index

--- list.js
+++ list.js
@@ -6,8 +6,8 @@ var input = prompt("what would you like to do ?")
 
 while(input !== "quit") {
     if(input === "list") {
-        todos.forEach(function(todo){
-            console.log(todo);
+        todos.forEach(function(todo, i){
+            console.log(i + ": " + todo);
         });
 } else if(input === "new") {
     const newTodo = prompt("enter new todo");
