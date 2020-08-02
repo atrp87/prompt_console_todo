7e183b4e29d98c09eba381e6e043121afca881d5
Andrew Peattie <drewpeattie@hotmail.com>
2020-08-02 13:27:06 +0100

    splice() & selected index

--- list.js
+++ list.js
@@ -2,18 +2,23 @@
 
 const todos = ["buy new turtle"];
 
-var input = prompt("what would you like to do ?")
+let input = prompt("what would you like to do ?")
 
 while(input !== "quit") {
     if(input === "list") {
+        console.log("*********")
         todos.forEach(function(todo, i){
             console.log(i + ": " + todo);
-        });
-} else if(input === "new") {
-    const newTodo = prompt("enter new todo");
-    todos.push(newTodo);
+    });
+        console.log("*********")
+    } else if(input === "new") {
+        const newTodo = prompt("enter new todo");
+        todos.push(newTodo);
+    } else if(input === "delete"){
+        let index = prompt("Enter index of item to delete");
+        todos.splice(index, 1); // delete one item
     }
 
     input = prompt("what would you like to do ?")
 }
-console.log("BYE")
+console.log("bye")
